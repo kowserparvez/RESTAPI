737f9269e8008cb3ca3bc050e75926bafff6adad
kp1439 <kowserparvez@gmail.com>
2018-03-22 17:22:43 -0400

    RestFul API GET Call

---
+++ restapi/src/test/java/com/qa/test/GetApiTest.java
@@ -0,0 +1,32 @@
+package com.qa.test;
+
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.qa.base.TestBase;
+import com.qa.client.RestClient;
+
+public class GetApiTest extends TestBase {
+	 TestBase testBase;
+	    String serviceUrl;
+	    String apiUrl;
+	    String url;
+	    RestClient restClient;
+	    @BeforeMethod
+	    public void setUp() {
+	        testBase = new TestBase();
+	        serviceUrl = prop.getProperty("URL");
+	        apiUrl = prop.getProperty("serviceURL");
+
+	        url = serviceUrl+apiUrl;
+	    }
+	    @Test
+	    public void getAPITest()throws IOException,ClientProtocolException {
+	        restClient = new RestClient();
+	        restClient.get(url);
+
+	    }
+}
